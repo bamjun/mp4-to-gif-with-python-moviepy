c005d24094ac94bde475a92ff90d0e71e58aab70
bamjun <khy5116@naver.com>
2024-11-13 14:28:35 +0900

    Feat:selection of resolution

--- README.md
+++ README.md
@@ -1,9 +1,15 @@
 # mp4 to gif
 
+python index.py --resolution 1  # for 320x240
+python index.py --resolution 2  # for 640x480
+python index.py --resolution 3  # for 1280x720
+python index.py --resolution 4  # for 1920x1080
+
+
 ```
 poetry install
 ```
 
 ```
-poetry run python index.py
+poetry run python index.py --resolution 1
 ```
--- index.py
+++ index.py
@@ -1,7 +1,25 @@
+import argparse
 from moviepy.editor import VideoFileClip
 
 mp4_file = 'test.mp4'
-gif_file = 'test.gif'
+gif_file = 'result_file/test.gif'
+
+# Add argument parsing for resolution selection
+parser = argparse.ArgumentParser(description='Select video resolution.')
+parser.add_argument('--resolution', type=int, choices=[1, 2], required=True,
+                    help='Choose resolution: 1 for 320x240, 2 for 640x480')
+args = parser.parse_args()
+
+# Set resolution based on user input
+if args.resolution == 1:
+    new_size = (320, 240)
+elif args.resolution == 2:
+    new_size = (640, 480)
+elif args.resolution == 3:
+    new_size = (1280, 720)
+elif args.resolution == 4:
+    new_size = (1920, 1080)
 
 clip = VideoFileClip(mp4_file)
-clip.write_videofile(gif_file, fps=10, codec='gif')
+clip = clip.resize(newsize=new_size)
+clip.write_videofile(gif_file, fps=5, codec='gif')
